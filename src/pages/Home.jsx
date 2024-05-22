e007bc47dd9ffa4859f1f1aeba9ca65258ad3fd6
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-05-22 19:25:14 +0700

    update feature
    update feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { usePenghuni } from "../hooks/usePenghuni"
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 import {
     Button,
@@ -17,6 +17,9 @@ const Home = () => {
     // eslint-disable-next-line no-unused-vars
     const [buttonstate, setButtonState] = useState(false)
 
+    const [disableButton, setDisableButton] = useState(false)
+    const [disableButtonCancel, setDisableButtonCancel] = useState(true)
+
     const [notif, setNotif] = useState("meminta akses");
     const [isLoadingNotif, setIsLoadingNotif] = useState(true)
     const [isErrorNotif, setIsErrorNotif] = useState("")
@@ -28,6 +31,7 @@ const Home = () => {
     const [pengunjung, setPengunjung] = useState("")
     const [kepentingan, setKepentingan] = useState("")
     const [penghuniId, setPenghuniId] = useState("")
+    const [pengunjungId, setPengunjungId] = useState("")
 
     const handleOpen = (penghuniId) => {
         setOpen(!open)
@@ -42,7 +46,6 @@ const Home = () => {
     const handleRequestMasuk = async (penghuniId, pengunjung, kepentingan) => {
 
         // setOpen(true)
-
         console.log("penghuniId: " + penghuniId)
         console.log("pengunjung: " + pengunjung)
         console.log("kepentingan: " + kepentingan)
@@ -53,7 +56,66 @@ const Home = () => {
             penghuniId: penghuniId,
             kepentingan,
             status: "memanggil",
-            isCalled: true
+            isCalled: true,
+            isOut: false
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then((response) => {
+            console.log(response.data)
+            setPengunjungId(response.data.id)
+            setInterval(async () => {
+
+                try {
+                    setIsErrorNotif("")
+                    const response_penghuni = await axios.get(
+                        `http://localhost:3000/api/v1/pengunjung/${response.data.id}`)
+                    console.log(response_penghuni)
+
+                    if (response_penghuni.data.status === "akses diterima") {
+                        setNotif("akses disetujui")
+                        setDisableButtonCancel(true)
+                    } else {
+                        setNotif("akses ditolak")
+                        setDisableButtonCancel(false)
+                    }
+                } catch (error) {
+
+                    console.error(error)
+                    setIsErrorNotif(error)
+                } finally {
+                    setIsLoadingNotif(false)
+                }
+
+            }, 60000)
+        }).catch(() => {
+            console.log('error while fetching')
+        }).finally(() => {
+            console.log('success')
+            setIsLoadingNotif(true)
+            
+        })
+
+        setDisableButton(true)
+       
+       
+    }
+
+    const handleRequestKeluar = async (penghuniId, pengunjung, kepentingan) => {
+        // setOpen(true)
+        console.log("penghuniId: " + penghuniId)
+        console.log("pengunjung: " + pengunjung)
+        console.log("kepentingan: " + kepentingan)
+
+        // status --> [memanggil, akses diterima, akses ditolak]
+        await axios.post('http://localhost:3000/api/v1/pengunjung', {
+            name: pengunjung,
+            penghuniId: penghuniId,
+            kepentingan,
+            status: "memanggil",
+            isCalled: true,
+            isOut: true
         }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -68,10 +130,12 @@ const Home = () => {
                         `http://localhost:3000/api/v1/pengunjung/${response.data.id}`)
                     console.log(response_penghuni)
 
-                    if (response_penghuni.data.isCalled === true) {
+                    if (response_penghuni.data.status === "akses diterima") {
                         setNotif("akses disetujui")
+                        setDisableButtonCancel(true)
                     } else {
                         setNotif("akses ditolak")
+                        setDisableButtonCancel(false)
                     }
                 } catch (error) {
 
@@ -81,7 +145,7 @@ const Home = () => {
                     setIsLoadingNotif(false)
                 }
 
-            }, 10000)
+            }, 60000)
 
             return () => clearInterval(interval);
 
@@ -92,8 +156,35 @@ const Home = () => {
             setIsLoadingNotif(true)
             
         })
+        setDisableButton(true)
+       
+    }
 
-        // setOpen(false)
+    const handleRefatching = async () => {
+
+    }
+
+    const handleCancel = async () => {
+        await axios.put(`http://localhost:3000/api/v2/pengunjung/${pengunjungId}`, {
+            status: "panggilan dibatalkan",
+            isCalled: false,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then((response) => {
+            console.log(response.data)
+        }).catch(() => {
+            console.log('error while fetching')
+        }).finally(() => {
+            console.log('success')
+            setIsLoadingNotif(true)
+            
+        })
+
+        setDisableButton(false)
+        setDisableButtonCancel(true)
+
     }
 
     const { data, loading, error } = usePenghuni('http://localhost:3000/api/v1/penghuni')
@@ -114,19 +205,21 @@ const Home = () => {
                 {data && data.map((penghuni) => {
                     return (
                         <>
-
-                            <button
-                                className="border-2 rounded-md w-full h-36 hover:bg-slate-400"
-                                key={penghuni.id}
-                                onClick={()=> handleOpen(penghuni.id)}
-                            >
-                                <div className="grid place-content-center ">
-                                    <p className="font-bold text-2xl  ">
-                                        {penghuni.alamat}
-                                    </p>
-                                </div>
-                            </button>
-
+                            {
+                                penghuni.isVeryfied && (
+                                    <button
+                                        className="border-2 rounded-md w-full h-36 hover:bg-slate-400"
+                                        key={penghuni.id}
+                                        onClick={()=> handleOpen(penghuni.id)}
+                                    >
+                                        <div className="grid place-content-center ">
+                                            <p className="font-bold text-2xl  ">
+                                                {penghuni.alamat}
+                                            </p>
+                                        </div>
+                                    </button>
+                                )
+                            }
                         </>
                     )
                 }
@@ -163,10 +256,15 @@ const Home = () => {
                     </div>
 
                     <Dialog open={open} >
-                                <DialogHeader>Form Pengunjung</DialogHeader>
+                        <DialogHeader className="flex justify-between">
+                            Form Pengunjung
 
-                                <DialogBody>
+                            <button className="w-6 h-6 p-2" onClick={handleOpen}>
+                                x
+                            </button>
 
+                        </DialogHeader>
+                            <DialogBody>
                                     <div className="flex flex-col gap-2">
                                         <Input label="Nama Pengunjung" onChange={e => setPengunjung(e.target.value)} />
                                         <Input label="Kepentingan" onChange={handleFillKepentingan} />
@@ -184,24 +282,30 @@ const Home = () => {
                                                 </>
                                         }
                                     </div>
-
-
-                                </DialogBody>
-                                <DialogFooter>
+                            </DialogBody>
+                            <DialogFooter>
+                                <div className="flex gap-2">
                                     <Button
                                         variant="text"
                                         color="red"
+                                        disabled={disableButtonCancel? true: false}
+
 
                                         className="mr-1"
-                                        onClick={handleOpen}
+                                        onClick={handleCancel}
                                     >
                                         <span>Cancel</span>
                                     </Button>
-                                    <Button variant="gradient" color="green" onClick={() => handleRequestMasuk(penghuniId, pengunjung, kepentingan)}>
-                                        <span>Meminta Akses</span>
+                                    <Button variant="gradient" color="green" disabled={disableButton ? true : false} onClick={() => {handleRequestMasuk(penghuniId, pengunjung, kepentingan), setIsOut(true)}}>
+                                        <span>Meminta Akses Masuk</span>
                                     </Button>
-                                </DialogFooter>
-                            </Dialog>
+
+                                    <Button variant="gradient" color="green" disabled={disableButton ? true : false} onClick={() => handleRequestKeluar(penghuniId, pengunjung, kepentingan)}>
+                                        <span>Meminta Akses Keluar</span>
+                                    </Button>
+                                </div>
+                            </DialogFooter>
+                    </Dialog>
 
 
                     <div
